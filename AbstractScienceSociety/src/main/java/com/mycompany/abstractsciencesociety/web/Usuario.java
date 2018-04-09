d89657abb1864b74c214fd1a3b57a2ab8126877d
DamiánRi <37169142+DamianRi@users.noreply.github.com>
2018-04-09 02:20:09 -0500

    Create Usuario.java

---
+++ AbstractScienceSociety/src/main/java/com/mycompany/abstractsciencesociety/web/Usuario.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.abstractsciencesociety.web;
+
+/**
+ *
+ * @author abstract
+ */
+public class Usuario {
+
+    private String nombre;
+    private String correo;
+    private String contraseña;
+    private String confirmacionContraseña;
+    private String carrera;
+    private int añoIngreso;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getConfirmacionContraseña() {
+        return confirmacionContraseña;
+    }
+
+    public void setConfirmacionContraseña(String confirmacionContraseña) {
+        this.confirmacionContraseña = confirmacionContraseña;
+    }
+    
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+    
+    public int getAñoIngreso() {
+        return añoIngreso;
+    }
+
+    public void setAñoIngreso(int añoIngreso) {
+        this.añoIngreso = añoIngreso;
+    }
+
+}
