7b1de27aa758b970c1e4b7c17c00d53a4e74bc80
edervs <eedervs.97@gmail.com>
2018-04-14 15:57:49 -0500

    Script para ingresar categorias

---
+++ SQL/insertacategorias.sql
@@ -0,0 +1,7 @@
+insert into categoria (nombre, descripcion) values ('Docencia','Referente a horarios e inscripciones');
+insert into categoria (nombre, descripcion) values ('Tramites','Referente a expedicion de documentos');
+insert into categoria (nombre, descripcion) values ('Egreso','Referente a titulacion , servicio social etc');
+insert into categoria (nombre, descripcion) values ('Servicio','Referente a Biblioteca , Publicaciones etc');
+insert into categoria (nombre, descripcion) values ('Vida en Cienicas','Referente a cultura y deporte');
+insert into categoria (nombre, descripcion) values ('Apoyo a estudiantes','Referente a becas , medico , etc');
+
